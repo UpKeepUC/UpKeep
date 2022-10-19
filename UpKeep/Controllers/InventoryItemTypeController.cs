e800519494d54d3bcf8be45e8e2efbe7e49b49fb
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2022-10-19 18:43:14 -0400

    Add type controllers

---
+++ UpKeep/Controllers/InventoryItemTypeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UpKeep.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryItemTypeController : ControllerBase
+    {
+        private readonly ILogger<InventoryItemTypeController> _logger;
+
+        public InventoryItemTypeController(ILogger<InventoryItemTypeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ UpKeep/Controllers/RoomTypeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UpKeep.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomTypeController : ControllerBase
+    {
+        private readonly ILogger<RoomTypeController> _logger;
+
+        public RoomTypeController(ILogger<RoomTypeController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
