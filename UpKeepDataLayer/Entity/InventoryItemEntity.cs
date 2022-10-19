20811b3bb0a6ac37070dd4f0e35826e069a83951
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2022-10-19 19:16:09 -0400

    Add InventoryItem Entity

---
+++ UpKeepDataLayer/Entity/InventoryItemEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpKeepData.Entity
+{
+    public class InventoryItemEntity
+    {
+        public int InventoryItemId { get; set; }
+        public int InventoryItemTypeId { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public string? InventoryItemCost { get; set; }
+        public int RoomId { get; set; }
+        public string? QRCodeId { get; set; }
+
+        public InventoryItemTypeEntity? InventoryItemType{get;set;} 
+        //public RoomEntity Room { get; set; }
+    }
+
+    public class InventoryItemTypeEntity
+    {
+        public int InventoryItemTypeId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<InventoryItemEntity>? InventoryItems { get; set; }
+    }
+}
