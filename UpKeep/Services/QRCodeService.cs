5eedc2900ebfd7e5722df404e8c70988433d6aec
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2023-02-16 20:47:02 -0500

    FIx custom qr code testing typo

--- UpKeep/Services/QRCodeService.cs
+++ UpKeep/Services/QRCodeService.cs
@@ -27,7 +27,7 @@ namespace UpKeep.Services
         { "X-RapidAPI-Key", _configuration.GetSection("QRCodeSecrets")["X-RapidAPI-Key"] },
         { "X-RapidAPI-Host", _configuration.GetSection("QRCodeSecrets")["X-RapidAPI-Host"] },
     },
-                Content = new StringContent("{\r\n    \"data\": \"https://google.com\",\r\n    \"image\": {\r\n        \"uri\": \"icon://appstore\",\r\n        \"modules\": true\r\n    },\r\n    \"style\": {\r\n        \"module\": {\r\n            \"color\": \"black\",\r\n            \"shape\": \"default\"\r\n        },\r\n        \"inner_eye\": {\r\n            \"shape\": \"default\"\r\n        },\r\n        \"outer_eye\": {\r\n            \"shape\": \"default\"\r\n        },\r\n        \"background\": {}\r\n    },\r\n    \"size\": {\r\n        \"width\": 200,\r\n        \"quiet_zone\": 4,\r\n        \"error_correction\": \"M\"\r\n    },\r\n    \"output\": {\r\n        \"filename\": \"qrcode\",\r\n        \"format\": \"png\"\r\n    }\r\n}")
+                Content = new StringContent("{\r\n    \"data\": \"https://google.com\",\r\n        \"style\": {\r\n        \"module\": {\r\n            \"color\": \"black\",\r\n            \"shape\": \"default\"\r\n        },\r\n        \"inner_eye\": {\r\n            \"shape\": \"default\"\r\n        },\r\n        \"outer_eye\": {\r\n            \"shape\": \"default\"\r\n        },\r\n        \"background\": {}\r\n    },\r\n    \"size\": {\r\n        \"width\": 200,\r\n        \"quiet_zone\": 4,\r\n        \"error_correction\": \"M\"\r\n    },\r\n    \"output\": {\r\n        \"filename\": \"qrcode\",\r\n        \"format\": \"png\"\r\n    }\r\n}")
                 {
                     Headers =
         {
