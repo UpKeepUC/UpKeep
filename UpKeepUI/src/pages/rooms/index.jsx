778ef92b9520d34622e33e8f10e20510720f0f2f
Rohan Patel <patel4r4@mail.uc.edu>
2023-03-19 19:51:40 -0400

    Rooms and Maintenance Form

--- UpKeepUI/src/pages/rooms/index.jsx
+++ UpKeepUI/src/pages/rooms/index.jsx
@@ -1,13 +1,91 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Modal } from "@mui/material";
+import { useState } from "react";
 import Header from "../../components/common/header";
+import InventoryTable from '../../components/InventoryTable/InventoryTable';
+import "../../index.css";
 
 const Rooms = () => {
+  const [selectedRoom, setSelectedRoom] = useState(null);
+
+  const handleRoomClick = (room) => {
+    setSelectedRoom(room);
+  };
+
+  const handleClose = () => {
+    setSelectedRoom(null);
+  };
+
+  const rooms100to140 = Array.from(Array(41), (_, i) => i + 100);
+  const rooms200to240 = Array.from(Array(41), (_, i) => i + 200);
+  const rooms300to340 = Array.from(Array(41), (_, i) => i + 300);
+
   return (
-  <Box m="20px">
-    <Box display="flex" justifyContent="space-between" alignItems="center">
-      <Header title="ROOMS" subtitle="" />
+    <Box m="20px">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Header title="ROOMS" subtitle="" />
+      </Box>
+      <Box mt="20px">
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Box display="flex" justifyContent="center">
+              <Box className="floor-plan">
+                {rooms100to140.map((room) => (
+                  <div
+                    key={room}
+                    className={`room room-${room}`}
+                    onClick={() => handleRoomClick(`Room ${room}`)}
+                  >
+                    <div className="room-number">{room}</div>
+                  </div>
+                ))}
+              </Box>
+              <Box className="floor-plan">
+                {rooms200to240.map((room) => (
+                  <div
+                    key={room}
+                    className={`room room-${room}`}
+                    onClick={() => handleRoomClick(`Room ${room}`)}
+                  >
+                    <div className="room-number">{room}</div>
+                  </div>
+                ))}
+              </Box><Box className="floor-plan">
+                {rooms300to340.map((room) => (
+                  <div
+                    key={room}
+                    className={`room room-${room}`}
+                    onClick={() => handleRoomClick(`Room ${room}`)}
+                  >
+                    <div className="room-number">{room}</div>
+                  </div>
+                ))}
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+      <Modal open={selectedRoom !== null} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "75%",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <h2>{selectedRoom}</h2>
+          <p><InventoryTable></InventoryTable></p>
+        </Box>
+      </Modal>
     </Box>
-  </Box>
   );
 };
 
