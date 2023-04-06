ba69f36af161ec7ec3657713bfb75b0846eb8d6b
handbrennan <handbd@mail.uc.edu>
2023-04-06 00:25:30 -0400

    Fixed Modal Formatting and Coloring

--- UpKeepUI/src/components/common/BasicModal/BasicModal.jsx
+++ UpKeepUI/src/components/common/BasicModal/BasicModal.jsx
@@ -1,44 +1,36 @@
-import React from 'react'
-import { Box, Modal, Typography, useTheme } from "@mui/material";
-import CommonButton from '../CommonButton/CommonButton';
+import React from "react";
+import { Box, Modal, Typography, useTheme, Grid, Stack } from "@mui/material";
+import CommonButton from "../CommonButton/CommonButton";
 import { tokens } from "../../../theme";
-import { modalStyles } from './styles';
-
-
+import { modalStyles } from "./styles";
 
 const BasicModal = ({ open, onClose, title, subTitle, content, onSubmit }) => {
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box sx={modalStyles.wrapper}>
+        <Typography variant="h4" component="h2">
+          {title}
+        </Typography>
+        <Typography sx={{ mt: 2, pb: 2 }} color="secondary">
+          {subTitle}
+        </Typography>
+        {content}
+        <Stack spacing={2} direction="row" sx={modalStyles.buttons}>
+          <CommonButton
+            variant="contained"
+            color="secondary"
+            onClick={onSubmit}
+          >
+            Submit
+          </CommonButton>
 
-    return (
-        <Modal open={open} onClose={onClose}  >
-            <Box sx={modalStyles.wrapper}>
-                <Typography
-                    variant="h6"
-                    component="h2"
-                >
-                    {title}
-                </Typography>
-                <Typography sx={{ mt: 2 }}>
-                    {subTitle}
-                </Typography>
-                    {content} 
-                <Box sx={modalStyles.buttons} >
-                    <CommonButton
-                        variant="contained"
-                        color="secondary"
-                        onClick={onSubmit}
-                    >
-                        Submit
-                    </CommonButton>
-                    <CommonButton 
-                    variant="contained"
-                    color="primary"
-                    onClick={onClose}
+          <CommonButton variant="contained" color="primary" onClick={onClose}>
+            Cancel
+          </CommonButton>
+        </Stack>
+      </Box>
+    </Modal>
+  );
+};
 
-                    >Cancel</CommonButton>
-                </Box>
-            </Box>
-        </Modal>
-    )
-}
-
-export default BasicModal
+export default BasicModal;
--- UpKeepUI/src/pages/inventory/view.jsx
+++ UpKeepUI/src/pages/inventory/view.jsx
@@ -1,233 +1,244 @@
-import React, { useState, useEffect} from "react";
-import dayjs from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { Box, TextField, MenuItem, Dialog, DialogTitle, DialogContent, FormControl, InputLabel, Grid } from "@mui/material";
-import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
+import React, { useState, useEffect } from "react";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import {
+  Box,
+  TextField,
+  MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  FormControl,
+  InputLabel,
+  Grid,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
 import Select from "@mui/material/Select";
-import demoqrcode from "./demoqrcode.png"
+import demoqrcode from "./demoqrcode.png";
 import axios from "axios";
 
- const InventoryView = () => {
+const InventoryView = () => {
+  const navigate = useNavigate();
+  const [inventoryItemTypes, setInventoryItemTypes] = useState([]);
+  const [roomModel, setRoomModel] = useState([]);
 
-    const navigate = useNavigate();
-    const [inventoryItemTypes, setInventoryItemTypes] = useState([]);
-    const [roomModel, setRoomModel] = useState([]);
+  const [qrCodeGenerated, setQRCodeGenerated] = useState(false);
 
-    const [qrCodeGenerated, setQRCodeGenerated] = useState(false);
+  const [inventoryItemId, setInventoryItemId] = useState(-1);
+  const [inventoryTypeId, setInventoryItemTypeId] = useState(-1);
+  const [roomId, setRoomId] = useState(-1);
+  const [purchaseDate, setPurchaseDate] = useState(dayjs("2000-01-01"));
+  const [inventoryItemCost, setInventoryItemCost] = useState("");
+  const [qrCodeId, setQrCodeId] = useState("");
 
-    const [inventoryItemId, setInventoryItemId] = useState(-1);
-    const [inventoryTypeId, setInventoryItemTypeId] = useState(-1);
-    const [roomId, setRoomId] = useState(-1);
-    const [purchaseDate, setPurchaseDate] = useState(dayjs('2000-01-01'));
-    const [inventoryItemCost, setInventoryItemCost] = useState("");
-    const [qrCodeId, setQrCodeId] = useState("");
+  const [responseReceived, setResponseReceived] = useState(false);
 
-    const [responseReceived, setResponseReceived] = useState(false);
-    
-    const handleInventoryItemChange = (event) => {
-      setInventoryItemTypeId(event.target.value); //inventory item type id 
+  const handleInventoryItemChange = (event) => {
+    setInventoryItemTypeId(event.target.value); //inventory item type id
+  };
+
+  const handleRoomChange = (event) => {
+    setRoomId(event.target.value);
+  };
+
+  const handleInventoryItemCost = (event) => {
+    setInventoryItemCost(event.target.value);
+  };
+
+  const handlePurchaseDate = (event) => {
+    console.log(event.$d);
+    setPurchaseDate(event.$d);
+  };
+
+  const handleDeleteClick = (event) => {
+    event.preventDefault();
+    const apiURL = process.env.REACT_APP_API_URL;
+
+    //build update model
+    const inventoryModel = {
+      InventoryItemId: inventoryItemId,
+      InventoryItemTypeId: inventoryTypeId,
+      InventoryItemCost: inventoryItemCost,
+      PurchaseDate: purchaseDate,
+      RoomId: roomId,
+      QrcodeId: qrCodeId,
     };
-  
-    const handleRoomChange = (event) => {
-      setRoomId(event.target.value);
-    }
-  
-    const handleInventoryItemCost = (event) => {
-      setInventoryItemCost(event.target.value);
-    }
-  
-    const handlePurchaseDate = (event) => {
-      console.log(event.$d);
-      setPurchaseDate(event.$d);
-    }
 
-    const handleDeleteClick = (event) => {
-      event.preventDefault();
-      const apiURL = process.env.REACT_APP_API_URL;
+    //submit post
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inventoryModel),
+    };
+    fetch(apiURL + "/InventoryItem/DeleteInventoryItem", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
 
-        //build update model
-      const inventoryModel = {
-        InventoryItemId: inventoryItemId,
-        InventoryItemTypeId: inventoryTypeId,
-        InventoryItemCost: inventoryItemCost,
-        PurchaseDate: purchaseDate,
-        RoomId: roomId,
-        QrcodeId: qrCodeId,
+    // setup onclose handler instead of refreshing page
+    navigate("/inventory");
+    window.location.reload();
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const apiURL = process.env.REACT_APP_API_URL;
+
+    //build update model
+    const inventoryModel = {
+      InventoryItemId: inventoryItemId,
+      InventoryItemTypeId: inventoryTypeId,
+      InventoryItemCost: inventoryItemCost,
+      PurchaseDate: purchaseDate,
+      RoomId: roomId,
+      QrcodeId: qrCodeId,
+    };
+
+    //submit post
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inventoryModel),
+    };
+    fetch(apiURL + "/InventoryItem/UpdateInventoryItem", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    // setup onclose handler instead of refreshing page
+    navigate("/inventory");
+    window.location.reload();
+  };
+
+  const apiURL = process.env.REACT_APP_API_URL;
+  useEffect(() => {
+    const getData = async () => {
+      const id = window.location.pathname.split("/")[2];
+      const response = await axios.get(
+        apiURL + "/InventoryItem/GetInventoryItemById?id=" + id
+      );
+      if (response.data) {
+        setInventoryItemId(response.data.inventoryItemId);
+        setInventoryItemTypeId(response.data.inventoryItemTypeId);
+        setRoomId(response.data.roomId);
+        setInventoryItemCost(response.data.inventoryItemCost);
+        setPurchaseDate(response.data.purchaseDate);
+        setQrCodeId(response.data.qrcodeId);
+        setResponseReceived(true);
       }
 
-      //submit post
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(inventoryModel)
+      const typeResponse = await axios.get(
+        apiURL + "/InventoryItemType/GetInventoryItemTypes"
+      );
+      if (typeResponse.data) {
+        setInventoryItemTypes(typeResponse.data);
+      }
+
+      const roomResponse = await axios.get(apiURL + "/Room/GetRooms");
+      if (roomResponse.data) {
+        setRoomModel(roomResponse.data);
+      }
     };
-      fetch(apiURL + '/InventoryItem/DeleteInventoryItem', requestOptions)
-        .then(response => response.json())
-        .then(data => console.log(data));
-      
-        // setup onclose handler instead of refreshing page
-        navigate('/inventory');
-        window.location.reload();
-    }
-  
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const apiURL = process.env.REACT_APP_API_URL;
+    getData();
+  }, []);
 
-        //build update model
-        const inventoryModel = {
-          InventoryItemId: inventoryItemId,
-          InventoryItemTypeId: inventoryTypeId,
-          InventoryItemCost: inventoryItemCost,
-          PurchaseDate: purchaseDate,
-          RoomId: roomId,
-          QrcodeId: qrCodeId,
-        }
+  const generateQRCode = () => {
+    setQRCodeGenerated(true);
+  };
 
-        //submit post
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(inventoryModel)
-      };
-        fetch(apiURL +'/InventoryItem/UpdateInventoryItem', requestOptions)
-          .then(response => response.json())
-          .then(data => console.log(data));
-        
-          // setup onclose handler instead of refreshing page
-          navigate('/inventory');
-          window.location.reload();
-      };
-
-    const apiURL = process.env.REACT_APP_API_URL;
-    useEffect(() => {
-        const getData = async() => {
-          const id = window.location.pathname.split('/')[2];
-          const response = await axios.get(apiURL +'/InventoryItem/GetInventoryItemById?id='+id);
-          if(response.data){
-            setInventoryItemId(response.data.inventoryItemId);
-            setInventoryItemTypeId(response.data.inventoryItemTypeId);
-            setRoomId(response.data.roomId);
-            setInventoryItemCost(response.data.inventoryItemCost);        
-            setPurchaseDate(response.data.purchaseDate);
-            setQrCodeId(response.data.qrcodeId);
-            setResponseReceived(true);
-          }
-
-          const typeResponse = await axios.get(apiURL + "/InventoryItemType/GetInventoryItemTypes");
-          if(typeResponse.data){
-            setInventoryItemTypes(typeResponse.data);
-          }
-
-          const roomResponse = await axios.get(apiURL + "/Room/GetRooms");
-          if(roomResponse.data){
-            setRoomModel(roomResponse.data)
-          }
-        }
-        getData();
-        }, []);
-
-    const generateQRCode = () => {
-        setQRCodeGenerated(true);
-    }
-  
-    return (
-      <Dialog open={responseReceived} m="20px">
-        <DialogTitle>INVENTORY ITEM</DialogTitle>
-        <DialogContent>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container direction="row" padding="10px">
-              <Grid item xs={6}>
-                <FormControl fullWidth>
-                  <InputLabel id="InventoryItemTypes">Inventory Item Type</InputLabel>
-                  <Select
-                    labelId="InventoryItemTypes"
-                    id="inventoryItemTypes"
-                    defaultValue={inventoryTypeId}
-                    label="Inventory Item Type"
-                    onChange={handleInventoryItemChange}
-                  >
+  return (
+    <Dialog open={responseReceived} m="20px">
+      <DialogTitle>INVENTORY ITEM</DialogTitle>
+      <DialogContent>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container direction="row" padding="10px">
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel id="InventoryItemTypes">
+                  Inventory Item Type
+                </InputLabel>
+                <Select
+                  labelId="InventoryItemTypes"
+                  id="inventoryItemTypes"
+                  defaultValue={inventoryTypeId}
+                  label="Inventory Item Type"
+                  onChange={handleInventoryItemChange}
+                >
                   {inventoryItemTypes.map((inventoryItemTypeModel) => (
-                    <MenuItem value={inventoryItemTypeModel.inventoryItemTypeId} >
+                    <MenuItem
+                      value={inventoryItemTypeModel.inventoryItemTypeId}
+                    >
                       {inventoryItemTypeModel.name}
                     </MenuItem>
                   ))}
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item xs={6}>
-                <FormControl fullWidth>
-                  <InputLabel id="Rooms">Rooms</InputLabel>
-                  <Select
-                    labelId="Rooms"
-                    id="rooms"
-                    defaultValue={roomId}
-                    label="Room"
-                    onChange={handleRoomChange}
-                  >
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel id="Rooms">Rooms</InputLabel>
+                <Select
+                  labelId="Rooms"
+                  id="rooms"
+                  defaultValue={roomId}
+                  label="Room"
+                  onChange={handleRoomChange}
+                >
                   {roomModel.map((roomModel) => (
-                    <MenuItem value={roomModel.roomId} >
+                    <MenuItem value={roomModel.roomId}>
                       {roomModel.roomLocation} - {roomModel.roomNumber}
                     </MenuItem>
                   ))}
-                  </Select>
-                </FormControl>           
-              </Grid>
+                </Select>
+              </FormControl>
             </Grid>
-            <Grid container direction="row">
-              <Grid item xs={12} padding="10px">
+          </Grid>
+          <Grid container direction="row">
+            <Grid item xs={12} padding="10px">
               <TextField
-                  label="Cost"
-                  required
-                  name="inventoryItemCost"
-                  fullWidth
-                  id="inventoryItemCost"                 
-                  defaultValue={inventoryItemCost}
-                  onChange={handleInventoryItemCost}
-                />
-              </Grid>
-              <Grid item xs={12} padding="10px">
+                label="Cost"
+                required
+                name="inventoryItemCost"
+                fullWidth
+                id="inventoryItemCost"
+                defaultValue={inventoryItemCost}
+                onChange={handleInventoryItemCost}
+              />
+            </Grid>
+            <Grid item xs={12} padding="10px">
               <LocalizationProvider fullWidth dateAdapter={AdapterDayjs}>
-                <DatePicker 
+                <DatePicker
                   label="Purchase Date"
-                  required
-                               
                   defaultValue={dayjs(purchaseDate)}
                   onChange={handlePurchaseDate}
                 />
-              </LocalizationProvider>              
-              </Grid>
+              </LocalizationProvider>
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Save
-            </Button>
-            <Button
-              color="error"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Delete
-            </Button>
-            {qrCodeGenerated &&
-                <img src={demoqrcode} alt='qr code here'/>
-            }
-                    
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Save
+          </Button>
+          <Button
+            color="error"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleDeleteClick}
+          >
+             Delete
+          </Button>
+          {qrCodeGenerated && <img src={demoqrcode} alt="qr code here" />}
         </Box>
-        </DialogContent>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-        
-      </Dialog>
-    );
-  };
-  
-  export default InventoryView;
+export default InventoryView;
