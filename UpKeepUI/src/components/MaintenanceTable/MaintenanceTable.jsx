fa22194b786736a18af841256a552d4ede652561
Rohan Patel <patel4r4@mail.uc.edu>
2023-04-10 15:51:25 -0400

    AddingMaintenanceTask (not fully functional)

--- UpKeepUI/src/components/MaintenanceTable/MaintenanceTable.jsx
+++ UpKeepUI/src/components/MaintenanceTable/MaintenanceTable.jsx
@@ -2,19 +2,9 @@ import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import DataTable from '../common/dataTable';
 
-// MaintenanceTaskId: 0,
-//                 MaintenanceTaskTypeId: 1,
-//                 MaintenanceTaskTypeModel: {},
-//                 Name: task,
-//                 Description: description,
-//                 MaintenanceTaskDueDate: new Date(dueDate),
-//                 MaintenanceTaskCompletedDate,
-
-const location = 'First floor: Room 101';
-
 const columns = [
     { field: 'maintenanceTaskId', headerName: 'Id', type: "number", headerAlign: "left", align: "left",},
-    { field: 'location', headerName: 'Location', flex: 1, valueGetter: () => 'Lobby' },
+    { field: 'roomModel', headerName: 'Location', flex: 1 },
     { field: 'name', headerName: 'Name', flex: 1 },
     { field: 'description', headerName: 'Description', flex: 1 },
     { field: 'maintenanceTaskDueDate', headerName: 'Task Due Date', flex: 1 }
