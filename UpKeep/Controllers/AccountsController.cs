87dedac3402d0f06293bbf7341c61496c6585e64
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2023-04-10 21:10:26 -0400

    Add creation, assigning, and checking of roles

--- UpKeep/Controllers/AccountsController.cs
+++ UpKeep/Controllers/AccountsController.cs
@@ -14,72 +14,140 @@ namespace UpKeep.Controllers
     [Route("api/[controller]/[action]")]
     public class AccountsController : ControllerBase
     {
-        private readonly IMapper _mapper; 
-        private readonly UserManager<User> _userManager; 
-        private readonly IConfigurationSection _jwtSettings; 
-        public AccountsController(IMapper mapper, UserManager<User> userManager, IConfiguration configuration) 
-        { 
-            _mapper = mapper; _userManager = userManager; 
-            _jwtSettings = configuration.GetSection("JWTSettings"); 
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfigurationSection _jwtSettings;
+        public AccountsController(IMapper mapper, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwtSettings = configuration.GetSection("JWTSettings");
         }
 
-        [HttpPost("Register")] 
-        public async Task<ActionResult> Register(UserRegistrationModel userModel) 
-        { 
-            var user = _mapper.Map<User>(userModel); 
-            var result = await _userManager.CreateAsync(user, userModel.Password); 
-            if (!result.Succeeded) 
-            { 
-                return Ok(result.Errors); 
-            } 
-            await _userManager.AddToRoleAsync(user, "Visitor"); 
-            return StatusCode(201); 
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register(UserRegistrationModel userModel)
+        {
+            var user = _mapper.Map<User>(userModel);
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+            if (!result.Succeeded)
+            {
+                return Ok(result.Errors);
+            }
+            await _userManager.AddToRoleAsync(user, "Visitor");
+            return StatusCode(201);
         }
 
-        [HttpPost("Login")] 
-        public async Task<IActionResult> Login(UserLoginModel userModel) 
-        { 
-            var user = await _userManager.FindByEmailAsync(userModel.Email); 
-            if (user != null && await _userManager.CheckPasswordAsync(user, userModel.Password)) 
-            { 
-                var signingCredentials = GetSigningCredentials(); 
-                var claims = GetClaims(user); 
-                var tokenOptions = GenerateTokenOptions(signingCredentials, await claims); 
-                var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions); 
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginModel userModel)
+        {
+            var user = await _userManager.FindByEmailAsync(userModel.Email);
+            if (user != null && await _userManager.CheckPasswordAsync(user, userModel.Password))
+            {
+                var signingCredentials = GetSigningCredentials();
+                var claims = GetClaims(user);
+                var tokenOptions = GenerateTokenOptions(signingCredentials, await claims);
+                var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
                 return Ok(token);
             }
             return Unauthorized("Invalid Authentication");
         }
 
-        private SigningCredentials GetSigningCredentials() 
-        { 
-            var key = Encoding.UTF8.GetBytes(_jwtSettings.GetSection("securityKey").Value); 
-            var secret = new SymmetricSecurityKey(key); 
-            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256); 
-        }
-        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims) 
-        { 
-            var tokenOptions = new JwtSecurityToken(
-                issuer: _jwtSettings.GetSection("validIssuer").Value, 
-                audience: _jwtSettings.GetSection("validAudience").Value, 
-                claims: claims, 
-                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_jwtSettings.GetSection("expiryInMinutes").Value)), 
-                signingCredentials: signingCredentials); 
-            return tokenOptions; 
-        }
-        private async Task<List<Claim>> GetClaims(User user) 
-        { 
-            var claims = new List<Claim> 
-            { 
-                new Claim(ClaimTypes.Name, user.Email) 
-            }; 
+        [HttpPost("CreateRole")]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            var role = new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                NormalizedName = roleName.ToUpper(),
+                Name = roleName
+            };
 
-            var roles = await _userManager.GetRolesAsync(user); 
-            foreach (var role in roles) 
-            { 
-                claims.Add(new Claim(ClaimTypes.Role, role)); 
-            } 
-            return claims; 
+            var identityRole = await _roleManager.CreateAsync(role);
+            if (identityRole != null && identityRole.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("AssignRole")]
+        public async Task<IActionResult> AssignRole(string username, string roleName)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                var roleExists = await _roleManager.RoleExistsAsync(roleName);
+
+                if (roleExists)
+                {
+                    var result = await _userManager.AddToRoleAsync(user, roleName);
+
+                    if (!result.Succeeded)
+                    {
+                        return BadRequest();
+                    }
+
+                    return Ok();
+
+                }
+                else
+                {
+                    return Unauthorized("Role does not exist");
+                }
+            }
+            else
+            {
+                return Unauthorized("User does not exist");
+            }
+        }
+
+        [HttpPost("CheckUserAdminRole")]
+        public async Task<IActionResult> CheckUserAdminRole(string username, string roleName)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if(user != null)
+            {
+                return Ok(await _userManager.IsInRoleAsync(user, roleName));
+
+            }
+
+            return Unauthorized("User does note exist");
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.GetSection("securityKey").Value);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _jwtSettings.GetSection("validIssuer").Value,
+                audience: _jwtSettings.GetSection("validAudience").Value,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_jwtSettings.GetSection("expiryInMinutes").Value)),
+                signingCredentials: signingCredentials);
+            return tokenOptions;
+        }
+        private async Task<List<Claim>> GetClaims(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
         }
     }
 }
